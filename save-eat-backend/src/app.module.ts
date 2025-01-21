24f8a99ef4f40a8418f90810f87d4aa1f6fa857e
Abdumajid  Rashidov <abdumajidrashidov44@gmail.com>
2025-01-21 23:45:03 +0000

    mongodb connected

--- save-eat-backend/src/app.module.ts
+++ save-eat-backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), // Load .env configuration
+    MongooseModule.forRoot(process.env.MONGO_URI!), // MongoDB URI from .env
+  ],
 })
 export class AppModule {}
